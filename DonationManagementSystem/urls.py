bfff3d6d30edec5cc2a917af047c7e7671f503b3
sharrmeen <sharmeenk2020@gmail.com>
2024-09-22 04:21:40 +0530

    added all_logins to url and created html page

--- DonationManagementSystem/urls.py
+++ DonationManagementSystem/urls.py
@@ -20,5 +20,6 @@ from donation.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",index,name="index")
+    path("",index,name="index"),
+    path("all_logins",all_logins,name="all_logins")
 ]
--- donation/views.py
+++ donation/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def all_logins(request):
+    return render(request,'all_logins.html')
