e0599d695bf6a156f911492d86e7f63ed3db3d06
sharrmeen <sharmeenk2020@gmail.com>
2024-09-28 19:00:38 +0530

    urls change

--- DonationManagementSystem/urls.py
+++ DonationManagementSystem/urls.py
@@ -21,5 +21,6 @@ from donation.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",index,name="index"),
-    path("all_logins",all_logins,name="all_logins")
+    path("all_logins",all_logins,name="all_logins"),
+    path("donor_login",donor_login,name="donor_login")
 ]
