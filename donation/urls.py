4a16321a84970997fac61b9690cef97e5f131e88
sharrmeen <sharmeenk2020@gmail.com>
2024-09-29 01:25:51 +0530

    changes

--- donation/urls.py
+++ donation/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
-from .views import authView, home
+from django.contrib import admin
+from django.urls import path
+from donation.views import *
 
 urlpatterns = [
-    path("", home, name = "home"),
-    path("signup/", authView, name = "authView"),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
+    path("",index,name="index"),
+    path("all_logins",all_logins,name="all_logins"),
 ]
