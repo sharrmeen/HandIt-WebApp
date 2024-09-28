f9eafaddc5a029ba0e8e923b2fd4258a7f656594
ankitacode19 <ankitacoder2004@gmail.com>
2024-09-28 19:10:54 +0530

    login frontend added

--- DonationManagementSystem/urls.py
+++ DonationManagementSystem/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from donation.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",index,name="index"),
-    path("all_logins",all_logins,name="all_logins")
+    path("all_logins",all_logins,name="all_logins"),
+    path("", include(("donation.urls", "donation"), "donation"))
 ]
--- donation/apps.py
+++ donation/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DonationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "donation"
+    label = "donation"
