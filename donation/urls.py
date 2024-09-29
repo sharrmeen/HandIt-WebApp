5c9453dd7e6465229097320add80cb13216b899a
sharrmeen <sharmeenk2020@gmail.com>
2024-09-29 21:38:53 +0530

    Create donor_login page

--- donation/urls.py
+++ donation/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",index,name="index"),
     path("all_logins",all_logins,name="all_logins"),
+    path("donor_login",donor_login,name="donor_login"),
     
 ]
