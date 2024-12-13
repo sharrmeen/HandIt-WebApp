c33abdbb0e6587dc2f4e5b1a75bd7d1efc3efef4
Sharmeen <sharmeenk2020@gmail.com>
2024-12-14 01:07:17 +0530

    fuckin bootstrap map again goddammit

--- DonationManagementSystem/settings.py
+++ DonationManagementSystem/settings.py
@@ -32,11 +32,6 @@ ALLOWED_HOSTS = ['Hand-It.onrender.com', 'localhost','127.0.0.1']
 
 # Application definition
 
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # Place it above all other middleware
-    ...
-]
 
 # Use Whitenoise to compress static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -55,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
