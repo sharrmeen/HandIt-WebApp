e275a5f1d3b297df3c673d56841a3215924191e2
Sharmeen <sharmeenk2020@gmail.com>
2024-12-14 00:44:46 +0530

    static directory path fixed

--- DonationManagementSystem/settings.py
+++ DonationManagementSystem/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../donation/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'donation', 'static')]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
