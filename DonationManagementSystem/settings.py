effe6da375c26a956c529f8f14898738f8e2eb6f
Sharmeen <sharmeenk2020@gmail.com>
2024-12-13 23:59:55 +0530

    whitenoise middleware

--- DonationManagementSystem/settings.py
+++ DonationManagementSystem/settings.py
@@ -32,6 +32,16 @@ ALLOWED_HOSTS = ['Hand-It.onrender.com', 'localhost','127.0.0.1']
 
 # Application definition
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # Place it above all other middleware
+    ...
+]
+
+# Use Whitenoise to compress static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 INSTALLED_APPS = [
     "donation",
     "django.contrib.admin",
@@ -113,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
