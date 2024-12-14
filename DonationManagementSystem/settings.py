316d011803feec721a131afcf2cf06f9ea50cc83
ankitacode19 <ankitacoder2004@gmail.com>
2024-12-14 21:53:16 +0530

    added css to reset password page

--- DonationManagementSystem/settings.py
+++ DonationManagementSystem/settings.py
@@ -38,6 +38,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 
+
 INSTALLED_APPS = [
     "donation",
     "django.contrib.admin",
@@ -118,6 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
